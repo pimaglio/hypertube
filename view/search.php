0a5888eba96b83e85ad1dc24f8e6febddc66a97f
Pierre Magliozzi <pimaglio@z1r10p6.le-101.fr>
2019-05-02 21:50:47 +0200

    ok

--- view/search.php
+++ view/search.php
@@ -112,33 +112,7 @@ include('header_connect.php');
                     </button>
                 </div>
             </div>
-    <p>
-      <label>
-        <input name=\"group1\" type=\"radio\" checked />
-        <span>Red</span>
-      </label>
-    </p>
-    <p>
-      <label>
-        <input name=\"group1\" type=\"radio\" />
-        <span>Yellow</span>
-      </label>
-    </p>
-    <p>
-      <label>
-        <input class=\"with-gap\" name=\"group1\" type=\"radio\"  />
-        <span>Green</span>
-      </label>
-    </p>
-    <p>
-      <label>
-        <input name=\"group1\" type=\"radio\" disabled=\"disabled\" />
-        <span>Brown</span>
-      </label>
-    </p>
-        
             ";
-
             ?>
 
         </form>
